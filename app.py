7e2bea82e1a193e58c0c024b6c999dcdbfb725d0
Wyatt James <wyattsamueljames@gmail.com>
2024-12-10 07:42:25 -0700

    chore: rename file

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world() -> str:
-    return "<p>Hello World</p>"
+    return "<p>Hello Boy</p>"
+
+
+@app.route("/<name>")
+def hello(name: str) -> str:
+    return f"Hello, {escape(name)}!"
 
 
 # def main() -> None:
-# 
-# 
+#
+#
 # if __name__ == "__main__":
 #     main()
