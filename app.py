dcaca44e33b1c96a9f1fa30e0f847f2197187bac
Wyatt James <wyattsamueljames@gmail.com>
2024-12-10 03:36:27 +0000

    feat: add hello_world

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world() -> str:
+    return "<p>Hello World</p>"
+
+
+# def main() -> None:
+# 
+# 
+# if __name__ == "__main__":
+#     main()
